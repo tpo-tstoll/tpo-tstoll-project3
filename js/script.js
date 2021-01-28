d7dcdbb8141a45f8768ebe37f05592623a6479ed
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-01-28 12:42:02 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ otherJobInput.style.display = 'none';
 //Display or hide other job input field based upon selection
 jobSelector.addEventListener('change', (e) => {
     e.preventDefault();
-    const jobSelected = e.target;
     if (e.target.value === 'other') {
         otherJobInput.style.display = '';
     } else {
@@ -34,9 +33,60 @@ shirtDesignSelector.addEventListener('change', (e) => {
     shirtColorSelector.disabled = false;
     for (let i = 0; i < shirtColorOptions.length; i++) {
         if(shirtColorOptions[i].getAttribute('data-theme') !== e.target.value){
-            shirtColorOptions[i].style.display = 'none';
+            shirtColorOptions[i].removeAttribute('selected');
+            shirtColorOptions[i].hidden= true;
         } else {
-            shirtColorOptions[i].style.display = '';
+            shirtColorOptions[i].setAttribute('selected', true);
+            shirtColorOptions[i].hidden = false;
         }
     }; 
 })
+
+//Daily Activity Cost
+
+const activitySelection = () => {
+    const activities = document.getElementById('activities');
+    let userCart = 0;
+    activities.addEventListener('change', (e) => {
+        let totalCost = document.getElementById('activities-cost');
+        let selectionCost = parseInt(e.target.getAttribute('data-cost'));
+        if (e.target.checked) {
+            userCart += selectionCost;
+        } else {
+            userCart -= selectionCost;
+        };
+        totalCost.innerHTML = `Total: $${userCart}`;
+    })
+}
+
+//Payment info
+const paymentSelector = () => {
+    const paymentMethod = document.getElementById('payment');
+    const creditCard = document.getElementById('credit-card');
+    const paypal = document.getElementById('paypal');
+    const bitcoin = document.getElementById('bitcoin');
+    paypal.hidden = true;
+    bitcoin.hidden = true;
+    paymentMethod.firstElementChild.nextElementSibling.setAttribute('selected', true);
+    paymentMethod.addEventListener('change', (e) => {
+        e.preventDefault();
+        if (e.target.value === paypal.id) {
+            paypal.hidden = false;
+            creditCard.hidden = true;
+            bitcoin.hiddent = true;
+        } else if (e.target.value === bitcoin.id) {
+            bitcoin.hidden = false;
+            creditCard.hidden = true;
+            paypal.hidden = true;
+        } else {
+            bitcoin.hidden = true;
+            creditCard.hidden = false;
+            paypal.hidden = true;
+        };
+    })
+}
+
+
+//Call functions
+activitySelection();
+paymentSelector();
